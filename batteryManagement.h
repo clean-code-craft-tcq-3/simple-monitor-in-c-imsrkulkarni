4e2e00f9407f38fb011c3217f469924644092aaf
imsrkulkarni <77853418+imsrkulkarni@users.noreply.github.com>
2022-05-10 17:59:48 +0530

    Create batteryManagement.h

---
+++ batteryManagement.h
@@ -0,0 +1,4 @@
+int isTemperatureInRange(float temperature);
+int isSocInRange(float soc);
+int isChargeRateInRange(float chargeRate);
+int batteryIsOk(float temperature, float soc, float chargeRate);
